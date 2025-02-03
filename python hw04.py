a3e66a84fd4cb4a64ed8ccf4554eaf77ec67d1ca
Sergo7480 <157243352+Sergo7480@users.noreply.github.com>
2025-02-03 19:57:41 +0200

    Update python hw04.py

--- python hw04.py
+++ python hw04.py
@@ -1,35 +1,51 @@
+import re
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Error: Contact not found."
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter user name."
+    return inner
+
 def parse_input(user_input):
     cmd, *args = user_input.strip().split()
     return cmd.lower(), args
 
+@input_error
 def add_contact(args, contacts):
-    name, phone = args
+    name, phone = args 
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args, contacts):
-    name, phone = args
+    name, phone = args  
     if name in contacts:
         contacts[name] = phone
         return "Contact updated."
     else:
-        return "Error: Contact not found."
+        raise KeyError  
 
+@input_error
 def show_phone(args, contacts):
-    name = args[0]
-    if name in contacts:
-        return f"Phone number for {name}: {contacts[name]}"
-    else:
-        return "Error: Contact not found."
+    name = args[0]  # Если нет аргументов → IndexError
+    return f"Phone number for {name}: {contacts[name]}"  
 
+@input_error
 def show_all(contacts):
     if not contacts:
         return "No contacts available."
     return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 def main():
-    contacts = {}
+    contacts = {}  # Храним контакты в словаре
     print("Welcome to the assistant bot!")
+
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
@@ -52,3 +68,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
