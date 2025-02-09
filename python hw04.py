33a3c585ecb9608c72401fb4d760d13f09cfcd46
Sergo7480 <157243352+Sergo7480@users.noreply.github.com>
2025-02-09 16:25:28 +0200

    Update python hw04.py

--- python hw04.py
+++ python hw04.py
@@ -12,29 +12,43 @@ def input_error(func):
             return "Enter user name."
     return inner
 
+def check_empty_input(func):
+    def inner(user_input, *args, **kwargs):
+        if not user_input.strip():
+            return None, []
+        return func(user_input, *args, **kwargs)
+    return inner
+
+@check_empty_input
 def parse_input(user_input):
     cmd, *args = user_input.strip().split()
     return cmd.lower(), args
 
 @input_error
 def add_contact(args, contacts):
-    name, phone = args 
+    if len(args) != 2: 
+        raise ValueError
+    name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 @input_error
 def change_contact(args, contacts):
-    name, phone = args  
+    if len(args) != 2:  
+        raise ValueError
+    name, phone = args
     if name in contacts:
         contacts[name] = phone
         return "Contact updated."
     else:
-        raise KeyError  
+        raise KeyError
 
 @input_error
 def show_phone(args, contacts):
-    name = args[0]  # Если нет аргументов → IndexError
-    return f"Phone number for {name}: {contacts[name]}"  
+    if len(args) != 1:
+        raise IndexError
+    name = args[0]
+    return f"Phone number for {name}: {contacts[name]}"
 
 @input_error
 def show_all(contacts):
@@ -43,12 +57,15 @@ def show_all(contacts):
     return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 def main():
-    contacts = {}  # Храним контакты в словаре
+    contacts = {} 
     print("Welcome to the assistant bot!")
 
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
+
+        if command is None:
+            continue
 
         if command in ["close", "exit"]:
             print("Good bye!")
